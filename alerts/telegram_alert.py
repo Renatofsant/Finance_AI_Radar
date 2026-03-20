88af06683ae72bd04860998131bd355951e83f35
RENATO SANTOS <114427740+Renatofsant@users.noreply.github.com>
2026-03-20 20:22:27 -0300

    Update telegram_alert.py

--- alerts/telegram_alert.py
+++ alerts/telegram_alert.py
@@ -1,13 +1,19 @@
 import requests
+import streamlit as st
 
-TOKEN = "8212900138:AAHKpjUWwur_FKLpdumWkZ2SL_xRxtw52Fw"
-CHAT_ID = "1831733779"
+# Puxa os dados das Secrets do Streamlit Cloud
+TOKEN = st.secrets.get("TELEGRAM_TOKEN")
+CHAT_ID = st.secrets.get("TELEGRAM_CHAT_ID")
 
 def enviar_alerta(msg):
-
+    if not TOKEN or not CHAT_ID:
+        return
+    
     url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
-
-    requests.post(url, data={
-        "chat_id": CHAT_ID,
-        "text": msg
-    })
+    try:
+        requests.post(url, data={
+            "chat_id": CHAT_ID,
+            "text": msg
+        })
+    except Exception as e:
+        print(f"Erro ao enviar Telegram: {e}")
